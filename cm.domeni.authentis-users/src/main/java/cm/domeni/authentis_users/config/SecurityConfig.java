f40058fd2cf5f10c6cc008c9307ddfa343ee5752
domeni.ngameni <stivedongawis@gmail.com>
2025-08-29 15:05:22 +0100

    refactor(auth-8): secure the endpoint to create a role

---
+++ cm.domeni.authentis-users/src/main/java/cm/domeni/authentis_users/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package cm.domeni.authentis_users.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static cm.domeni.authentis_users.config.Scopes.ROLE_CREATE;
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests(authorize -> authorize
+                .requestMatchers(HttpMethod.POST, "/register").permitAll()
+                .requestMatchers(HttpMethod.POST, "/role").hasAuthority(ROLE_CREATE)
+                .anyRequest()
+                    .denyAll()
+            )
+            .oauth2ResourceServer(oauth2 -> oauth2.jwt(withDefaults()));
+        return http.build();
+    }
+}
