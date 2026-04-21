95c4f1e99a92dcc73b16b0c9b163ce5540ba6be9
domeni.ngameni <stivedongawis@gmail.com>
2026-04-21 18:06:49 +0100

    build(auth-14): config docker

--- cm.domeni.authentis-users/settings.gradle.kts
+++ cm.domeni.authentis-users/settings.gradle.kts
@@ -1,6 +1,6 @@
 rootProject.name = "cm.domeni.authentis-users"
 
-val defaultKapitaPlatformLibsDir = file("../../../domeni/cm.domeni.kapita/platform-libs")
+val defaultKapitaPlatformLibsDir = file("../../cm.domeni.kapita/platform-libs")
 val kapitaPlatformLibsDir =
     startParameter.projectProperties["kapitaPlatformLibsDir"]?.let(::file)
         ?: System.getenv("KAPITA_PLATFORM_LIBS_DIR")?.let(::file)
